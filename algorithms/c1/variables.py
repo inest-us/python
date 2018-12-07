c50d95efbb23a43be7ff2e2e35759f836b5377ca
Hieu Nguyen <hieumum@gmail.com>
2018-12-07 13:11:50 -0800

    basic operators and variables

---
+++ algorithms/c1/variables.py
@@ -0,0 +1,11 @@
+# A Python variable is created when a name is used for the first time on the left-hand side of
+# an assignment statement.
+
+the_sum = 0
+print(the_sum) #0
+
+the_sum = the_sum + 1
+print(the_sum) #1
+
+the_sum = True
+print(the_sum) #True
