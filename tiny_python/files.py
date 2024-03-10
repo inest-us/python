7baf262d3d779a2ecac8b890806dfbdb7a5e77b0
inest-us <hieung@live.com>
2024-03-09 23:05:47 -0600

    clean up

--- tiny_python/files.py
+++
@@ -1,8 +0,0 @@
-with open('./tiny_python/names.txt', 'w') as fout:
-    fout.write('Paul\r\nJohn\n')
-    fout.writelines(['Ringo\n', 'George\n'])
-
-with open('./tiny_python/names.txt') as fin:
-    for line in fin:
-        print (repr(line))
-
--- tiny_python/tuples.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple 
-Member = namedtuple('Member', 'name, instrument, birth_year') 
-member3 = Member(' George', 'Guitar', 1943) 
-
-#We can access members by position or name (name allows us to be more explicit): 
-print(member3[0]) #'George'
-print(member3.name) #'George'
-
